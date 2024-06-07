1a128961745a0e59e4d4c1acdbabde3c86db6033
Okwel Edgar Mark <mcedgars190@gmail.com>
2024-06-07 20:44:20 +0300

    This is my second commit

---
+++ okwel_edgar_mark_day2.py
@@ -0,0 +1,76 @@
+# grade = 88
+# if grade >= 90:
+#     print("Excellent")
+# elif grade >= 80:
+#     print("very good")
+# elif grade >= 70:
+#     print("good")
+# else:
+#     print("not good")
+    
+    
+  #  write a python script to determine the price of a movie based on age
+  #person < 13, discount ==1000.
+  #person 13<= person<=18, pays full price
+  #otherwise, senior citizen pays 5000
+   
+# person = 12
+# if person < 13:
+#     print("the discount is 1000")
+# elif person>13 and person<18:
+#     print("pays full amount")
+# else:
+#     print("pays 5000")
+#..............................................................
+#...........................................................
+#............................................................
+#.....................
+    
+
+# price = 3000
+
+# # Prompt the user to input their age
+# person = int(input("Please enter your age: "))
+
+# # Determine the discount or payment amount based on age
+# if person <= 13:
+#     print(price-1000)
+# elif person>13 and person<=18:
+#     print(price)
+# elif person>18 and person<=64:
+#     print(price+500)
+# else:
+#     print("Pays 5000")
+
+#..............................................................
+#...........................................................
+#............................................................
+#...........................................................
+#example three 
+#loops (for, while)
+#we shalll see examples of..........
+#........list
+#........tuple
+#........dictionary
+#........set
+#........string
+#example 3.1(list)
+
+
+# phones = ['iphone', 'samsung', 'itel', 'tecno']
+# for phone in phones:
+#     #print(phone)
+#     print(phones[0])
+    
+    
+    
+#exercise
+# colors = ["sky blue", "yellow", "orange"]
+# for color in colors:
+#     print(color)
+    
+
+count = 5
+while count >= 1:    
+    print("count 5 to 1:", count)
+    count -= 1
